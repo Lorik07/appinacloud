3ba1c5471e5c7f73528400194cd7aed6f45c366a
Lorik Mushica <lorik.mushica@ict-campus.net>
2024-05-31 11:12:24 +0200

    yurr

---
+++ src/functions/postltms.js
@@ -0,0 +1,23 @@
+const { app, output } = require('@azure/functions');
+ 
+const cosmosOutput = output.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+ 
+});
+ 
+app.http('createItem', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    extraOutputs: [cosmosOutput],
+    route: 'items/post',
+    handler: async (request, context) => {
+        const newItem = await request.json();
+       
+        context.extraOutputs.set(cosmosOutput, newItem);
+       
+       
+    }
+});
+ 
